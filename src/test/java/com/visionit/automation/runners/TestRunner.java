aa14573be4387842efcb672f0072b3c4f36f5251
truptibankar <truptibankar12@gmail.com>
2020-10-02 16:19:29 +0530

    stepdefs generated

--- src/test/java/com/visionit/automation/runners/TestRunner.java
+++ src/test/java/com/visionit/automation/runners/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/resources/Feature",
+		features = "src/test/resources/features",
 		glue = "stepdefs",
 		tags ="",
 		plugin = {"pretty",
